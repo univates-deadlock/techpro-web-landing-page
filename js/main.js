6167e6542b051a7c74d93b15daed8331e6ffb50a
matbdev <mateuscarnielbrambilla@gmail.com>
2026-09-19 09:13:37 -0300

    refactor(institutional): extract repeated stats and purpose cards

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 import "./components/header.js";
 import "./components/footer.js";
+
+// Institutional
+import "./components/institutional/stat-card.js";
+import "./components/institutional/purpose-card.js";
